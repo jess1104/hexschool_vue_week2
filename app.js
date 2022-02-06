7861b9bd1c1707c23c0c5f533e9ad4b614c51f66
jess1104 <jess1104jess1104@gmail.com>
2022-02-06 21:30:46 +0800

    check api done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
-const url = 'https://vue3-course-api.hexschool.io/v2'
+const site = 'https://vue3-course-api.hexschool.io/v2'
 
 createApp({
     data(){
@@ -10,17 +10,19 @@ createApp({
             }
         }
     },
-    methods:{
+    methods:{   
         login(){
-           axios.post(`${url}/admin/signin`,this.user).then((res)=>{
-               const { token, expired } = res.data;
-                // 存入cookie
-                document.cookie = `jessToken=${token}; expires=${ new Date(expired) }; `;
-                window.location = 'products.html';
-           }) .catch((err)=>{
-            console.dir(err);
-            alert(err.data.message)
-          }) 
+            const url = `${site}/admin/signin`;
+            axios.post(url,this.user).then((res)=>{
+                console.log(res.data);
+                const { token, expired } = res.data;
+                    // 存入cookie
+                    document.cookie = `jessToken=${token}; expires=${ new Date(expired) }; `;
+                    window.location = 'products.html';
+            }).catch((err)=>{
+                console.dir(err);
+                alert(err.data.message)
+            }) 
         }
     }
 }).mount('#app');
